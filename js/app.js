dbaf3473b6cd48d68348ca343c8f27cdc7ae8baa
xyyojl <m13631897256@163.com>
2019-04-14 13:14:52 +0800

    实现删除任务和修改任务

--- js/app.js
+++ js/app.js
@@ -3,22 +3,41 @@
 
 	let vm = new Vue({
 		el:'#todoapp',
+		directives:{
+			focus(el,bindings){
+				// 当点击当前li时让内部的输入框获取焦点
+				if(bindings.value){
+					el.focus(); // 获取焦点
+				}
+			}
+		},
 		data:{
 			todos:[
 				{isSelected:false,title:'吃饭'},
 				{isSelected:true,title:'睡觉'},
 				{isSelected:false,title:'购物'},
 			],
-			title:''
+			title:'',
+			cur:''
 		},
 		methods:{
 			addTodo(){
-				console.log(1);
+				if(!this.title) return;
 				this.todos.unshift({
 					isSelected : false,
 					title : this.title
 				})
 				this.title = ''
+			},
+			removeTodo(todo){
+				console.log(1);
+				this.todos = this.todos.filter(item=>item!==todo);
+			},
+			remember(todo){
+				this.cur = todo;
+			},
+			cancel(){
+				this.cur = ''
 			}
 		},
 		computed:{ // 放在computed中最后也会放在vm上，不能和methods与data重名
