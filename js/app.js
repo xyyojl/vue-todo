8281dbdae1a2995c7afda447ccc7fdca3442eb91
xyyojl <m13631897256@163.com>
2019-04-14 12:36:50 +0800

    遍历数据，实现添加任务

--- js/app.js
+++ js/app.js
@@ -1,6 +1,26 @@
 (function (window) {
 	'use strict';
 
-	
+	let vm = new Vue({
+		el:'#todoapp',
+		data:{
+			todos:[
+				{isSelected:false,title:'吃饭'},
+				{isSelected:true,title:'睡觉'},
+				{isSelected:false,title:'购物'},
+			],
+			title:''
+		},
+		methods:{
+			addTodo(){
+				console.log(1);
+				this.todos.unshift({
+					isSelected : false,
+					title : this.title
+				})
+				this.title = ''
+			}
+		}
+	})
 
 })(window);
